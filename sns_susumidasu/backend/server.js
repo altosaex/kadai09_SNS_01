eda81d62ab475d2bb9e7ad98d4a1c8158b7e65ad
altosaex <147964457+altosaex@users.noreply.github.com>
2024-01-06 00:42:31 +0900

    Update server.js

--- sns_susumidasu/backend/server.js
+++ sns_susumidasu/backend/server.js
@@ -3,6 +3,7 @@ const app = express();
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const uploadRoute = require("./routes/upload");
 const PORT = 4000;
 const mongoose = require("mongoose");
 require("dotenv").config();
@@ -21,6 +22,7 @@ app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/upload", uploadRoute);
 
 app.get("/", (req, res) => {
 	res.send("hello express");
