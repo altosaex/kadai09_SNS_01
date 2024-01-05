846610f4eb11347e1c9df48b0f5805de674e1af7
altosaex <147964457+altosaex@users.noreply.github.com>
2024-01-05 19:32:31 +0900

    Update AuthContext.js

--- sns_susumidasu/frontend/src/state/AuthContext.js
+++ sns_susumidasu/frontend/src/state/AuthContext.js
@@ -3,10 +3,22 @@ import AuthReducer from "./AuthReducer";
 
 //最初のユーザー状態を定義
 const initialState = {
-	user: null,
-	isFetching: false,
-	error: false,
+	// user: null,
+	user: {
+		_id: "6592c5e00477106d5a9f2d45",
+		username: "altosaex",
+		email: "sae.coach@gmail.com",
+		password: "Y6jxkVHT9",
+		profilePicture: "",
+		coverPicture: "",
+		followers: [],
+		followings: [],
+		isAdmin: false,
+	},
+		isFetching: false,
+		error: false,
 };
+
 
 //状態をグローバルに管理する
 export const AuthContext = createContext(initialState);
