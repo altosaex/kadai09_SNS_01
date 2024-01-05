9be388efd7a13080f35c22e1dee20b42ba462441
altosaex <147964457+altosaex@users.noreply.github.com>
2024-01-05 19:55:34 +0900

    Update Post.jsx

--- sns_susumidasu/frontend/src/components/post/Post.jsx
+++ sns_susumidasu/frontend/src/components/post/Post.jsx
@@ -1,16 +1,18 @@
 import "./Post.css";
 import { MoreVert } from '@mui/icons-material';
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 // import { Users } from "../../dummyDate";
 import axios from "axios";
 import { format } from 'timeago.js';
 import { Link } from 'react-router-dom';
+import { AuthContext } from "../../state/AuthContext";
 
 export default function Post( {post} ) {
 	const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
 	const [like, setLike] = useState(post.likes.length);
 	const [isLiked, setIsLiked] = useState(false);
 	const [user, setUser] = useState({});
+	const {user: currentUser} = useContext(AuthContext);
 
 	useEffect(() => {
 		const fetchUser = async () => {
@@ -21,7 +23,15 @@ export default function Post( {post} ) {
 		fetchUser();
 	}, [post.userId]);
 
-	const handleLike = () => {
+	const handleLike = async () => {
+		try {
+			//いいねのAPIを叩いていく
+			await axios.put(`/posts/${post._id}/like`, {userId: currentUser._id });
+		} catch (err) {
+			console.log(err);
+
+		}
+
 		setLike(isLiked ? like - 1 : like + 1);
 		setIsLiked(!isLiked);
 	};
@@ -32,7 +42,10 @@ export default function Post( {post} ) {
 				<div className="postTop">
 					<div className="postTopLeft">
 						<Link to={`/profile/${user.username}`}>
-					<img src={user.profilePicture || PUBLIC_FOLDER + "/person/noAvatar.png"} alt="" className='postProfileImg'/>
+					<img src={
+						user.profilePicture 
+						? PUBLIC_FOLDER + user.profilePicture 
+						: PUBLIC_FOLDER + "/person/noAvatar.png"} alt="" className='postProfileImg'/>
 						</Link>
 					<span className="postUsername">{user.username}</span>
 					<span className="postDate">{format(post.createdAt)}</span>
