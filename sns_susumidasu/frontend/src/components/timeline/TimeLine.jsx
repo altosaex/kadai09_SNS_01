5e26ab653059459869bdab130724546b01aae2f5
altosaex <147964457+altosaex@users.noreply.github.com>
2024-01-05 19:55:13 +0900

    Update TimeLine.jsx

--- sns_susumidasu/frontend/src/components/timeline/TimeLine.jsx
+++ sns_susumidasu/frontend/src/components/timeline/TimeLine.jsx
@@ -4,20 +4,24 @@ import Share from '../share/Share';
 import Post from '../post/Post';
 // import {Posts} from "../../dummyDate";
 import axios from "axios";
+import { useContext } from 'react';
+import { AuthContext } from '../../state/AuthContext';
 
 export default function TimeLine({ username }) {
 	const [posts, setPosts] = useState([]);
 
+	const {user} = useContext(AuthContext);
+
 	useEffect(() => {
 		const fetchPosts = async () => {
 			const response = username 
-			? await axios.get(`/posts/profile/${username}`) 
-			: await axios.get("/posts/timeline/6592c5e00477106d5a9f2d45");
+			? await axios.get(`/posts/profile/${username}`) //プロフィールの場合
+			: await axios.get(`/posts/timeline/${user._id}`); //ホームの場合
 			// console.log(response);
 			setPosts(response.data);
 		};
 		fetchPosts();
-	}, [username]);
+	}, [username, user._id]);
 
 	return (
 	<div className='timeline'>
